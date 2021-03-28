fbaf77892b66fb1426e3a0629c9f596ae8a6e5ab
NWpela <81514199+NWpela@users.noreply.github.com>
2021-03-28 13:38:28 +0200

    Jeux de dames

---
+++ class_game2.js
@@ -0,0 +1,346 @@
+class Game {
+	constructor(player1, player2) {
+		this.player1 = player1; //en bas
+		this.player2 = player2; //en haut
+
+		this.eaten1 = 0;
+		this.eaten2 = 0;
+
+		this.status = 'stoped';
+		this.turn = 'player1';
+
+		this.readyPlayer1 = false;
+		this.readyPlayer2 = false;
+
+		this.blows = [];
+
+		this.data_game = [[-1, 0, -1, 0, -1, 0, -1, 0, -1, 0],
+					 	[0, -1, 0, -1, 0, -1, 0, -1, 0, -1],
+					 	[-1, 0, -1, 0, -1, 0, -1, 0, -1, 0],
+					 	[0, -1, 0, -1, 0, -1, 0, -1, 0, -1],
+					 	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+					 	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+					 	[1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+					 	[0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+					 	[1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+					 	[0, 1, 0, 1, 0, 1, 0, 1, 0, 1]];
+
+		};
+
+	getPlayer(player) {
+		switch (player) {
+			case this.player1:
+				return 'player1'
+				break
+			case this.player2:
+				return 'player2'
+		}
+	}
+
+	validSelection(player, pi, pj) {
+		if (player == this.player1 && this.data_game[pi][pj] >= 1 || player == this.player2 && this.data_game[pi][pj] <= -1) {
+			return true
+		}
+		else {
+			console.log('[validSelection]: '+player+ ' '+pi, + ' '+pj+' case: '+this.data_game[pi][pj]);
+			return false
+		}
+	}
+
+	NbBetween(player, p1i, p1j, p2i, p2j) {
+		switch (player) {
+			case this.player1:
+				var posList = [];
+				var n = 0;
+				var l = Math.abs(p2i-p1i);
+				var di = (p2i-p1i)/l;
+				var dj = (p2j-p1j)/l;
+				var p0i = p1i;
+				var p0j = p1j;
+				while (n<l) {
+					if (this.data_game[p0i][p0j] <= -1) {
+						posList.push({'i': p0i, 'j': p0j});
+						console.log('[NbBetween]: '+'finded');
+					}
+					n++;
+					p0i += di;
+					p0j += dj;
+				};
+				return posList;
+				break;
+
+			case this.player2:
+				var posList = [];
+				var n = 0;
+				var l = Math.abs(p2i-p1i);
+				var di = (p2i-p1i)/l;
+				var dj = (p2j-p1j)/l;
+				var p0i = p1i;
+				var p0j = p1j;
+				while (n<l) {
+					if (this.data_game[p0i][p0j] >= 1) {
+						posList.push({'i': p0i, 'j': p0j});
+						console.log('[NbBetween]: '+'finded');
+					}
+					n++;
+					p0i += di;
+					p0j += dj;
+				};
+				return posList;
+		}
+	}
+
+	move(player,p1i,p1j,p2i,p2j) {
+		if (this.validSelection(player, p1i, p1j)) {
+			//sélection valide
+			if (this.data_game[p2i][p2j] == 0) {
+				//cible vide
+
+				//cas joueur 1
+				if (player == this.player1) {
+					if (this.data_game[p1i][p1j] == 1) {
+						//pion normal
+						if (Math.abs(p2i - p1i) == 2 && Math.abs(p2j - p1j) == 2) {
+							//coup attaque
+							if (this.data_game[(p2i+p1i)/2][(p2j+p1j)/2] <= -1) {
+								//présence d'un pion ennemi
+								this.data_game[(p2i+p1i)/2][(p2j+p1j)/2] = 0;
+								this.data_game[p1i][p1j] = 0;
+								this.eaten1 += 1;
+								if (p2i == 0) {
+									//transformation en dame
+									this.data_game[p2i][p2j] = 2;
+									if (this.isWon(player)) {
+										return {'player': 'player1', 'issue': 'eaten', 'transform': true, 'posEaten': {'i':(p2i+p1i)/2, 'j':(p2j+p1j)/2}, 'pieceType': 'piece', 'win': 'player1'};
+									}
+									return {'player': 'player1', 'issue': 'eaten', 'transform': true, 'posEaten': {'i':(p2i+p1i)/2, 'j':(p2j+p1j)/2}, 'pieceType': 'piece'};
+								}
+								else {
+									//pas de transformation
+									this.data_game[p2i][p2j] = 1;
+									if (this.isWon(player)) {
+										return {'player': 'player1', 'issue': 'eaten', 'transform': false, 'posEaten': {'i':(p2i+p1i)/2, 'j':(p2j+p1j)/2}, 'pieceType': 'piece', 'win': 'player1'};
+									}
+									return {'player': 'player1', 'issue': 'eaten', 'transform': false, 'posEaten': {'i':(p2i+p1i)/2, 'j':(p2j+p1j)/2}, 'pieceType': 'piece'};
+								}
+							}
+							else {console.log('[Move]: No adverse piece at '+(p2i+p1i)/2+' '+(p2j+p1j)/2); return false;}
+						}
+						if (p1i - p2i == 1 && Math.abs(p2j - p1j) == 1) {
+							//coup simple
+							this.data_game[p1i][p1j] = 0;
+							if (p2i == 0) {
+								//transformation en dame
+								this.data_game[p2i][p2j] = 2;
+								return {'player': 'player1', 'issue': 'simple', 'transform': true, 'pieceType': 'piece'};
+							}
+							else {
+								//pas de transformation
+								this.data_game[p2i][p2j] = 1;
+								return {'player': 'player1', 'issue': 'simple', 'transform': false, 'pieceType': 'piece'};
+							}
+						}
+						//si aucun des cas dessus avec pion simple
+						console.log('[Move]: Impossible move with simple piece');
+						return false;
+					}
+					if (this.data_game[p1i][p1j] == 2) {
+						//coup avec dame
+						if (Math.abs(p1i-p2i) == Math.abs(p1j-p2j)) {
+							//le coup est valide
+							var posList = this.NbBetween(player, p1i, p1j, p2i, p2j);
+							if (posList.length == 0) {
+								//pas de pion mangé
+								this.data_game[p1i][p1j] = 0;
+								this.data_game[p2i][p2j] = 2;
+								return {'player': 'player1', 'issue': 'simple', 'transform': false, 'pieceType': 'dame'};
+							}
+							if (posList.length == 1) {
+								//présence d'un pion
+								this.data_game[p1i][p1j] = 0;
+								this.data_game[p2i][p2j] = 2;
+								this.data_game[posList[0].i][posList[0].j] = 0;
+								this.eaten1 += 1;
+								if (this.isWon(player)) {
+									return {'player': 'player1', 'issue': 'eaten', 'transform': false, 'posEaten': posList[0], 'pieceType': 'dame', 'win': 'player1'};
+								}
+								return {'player': 'player1', 'issue': 'eaten', 'transform': false, 'posEaten': posList[0], 'pieceType': 'dame'};
+							}
+							//trop de pions présents
+							console.log('[Move]: Too much pieces NbBetween '+p1i+','+p1j+' and '+p2i+','+p2j);
+							return false;
+						}
+						console.log('[Move]: Impossible move with a dame');
+						return false;
+					}
+					console.log('[Move]: Invalid selection (inside move) at '+p1i+','+p1j);
+					return false;
+				}
+
+				//cas joueur 2
+				if (player == this.player2) {
+					if (this.data_game[p1i][p1j] == -1) {
+						//pion normal
+						if (Math.abs(p2i - p1i) == 2 && Math.abs(p2j - p1j) == 2) {
+							//coup attaque
+							if (this.data_game[(p2i+p1i)/2][(p2j+p1j)/2] >= 1) {
+								//présence d'un pion ennemi
+								this.data_game[(p2i+p1i)/2][(p2j+p1j)/2] = 0;
+								this.data_game[p1i][p1j] = 0;
+								this.eaten2 += 1;
+								if (p2i == 9) {
+									//transformation en dame
+									this.data_game[p2i][p2j] = -2;
+									if (this.isWon(player)) {
+										return {'player': 'player2', 'issue': 'eaten', 'transform': true, 'posEaten': {'i':(p2i+p1i)/2, 'j':(p2j+p1j)/2}, 'pieceType': 'piece', 'win': 'player2'};
+									}
+									return {'player': 'player2', 'issue': 'eaten', 'transform': true, 'posEaten': {'i':(p2i+p1i)/2, 'j':(p2j+p1j)/2}, 'pieceType': 'piece'};
+								}
+								else {
+									//pas de transformation
+									this.data_game[p2i][p2j] = -1;
+									if (this.isWon(player)) {
+										return {'player': 'player2', 'issue': 'eaten', 'transform': false, 'posEaten': {'i':(p2i+p1i)/2, 'j':(p2j+p1j)/2}, 'pieceType': 'piece', 'win': 'player2'};
+									}
+									return {'player': 'player2', 'issue': 'eaten', 'transform': false, 'posEaten': {'i':(p2i+p1i)/2, 'j':(p2j+p1j)/2}, 'pieceType': 'piece'};
+								}
+							}
+							else {console.log('[Move]: No adverse piece at '+(p2i+p1i)/2+' '+(p2j+p1j)/2); return false;}
+						}
+						if (p1i - p2i == -1 && Math.abs(p2j - p1j) == 1) {
+							//coup simple
+							this.data_game[p1i][p1j] = 0;
+							if (p2i == 9) {
+								//transformation en dame
+								this.data_game[p2i][p2j] = -2;
+								return {'player': 'player2', 'issue': 'simple', 'transform': true, 'pieceType': 'piece'};
+							}
+							else {
+								//pas de transformation
+								this.data_game[p2i][p2j] = -1;
+								return {'player': 'player2', 'issue': 'simple', 'transform': false, 'pieceType': 'piece'};
+							}
+						}
+						//si aucun des cas dessus avec pion simple
+						console.log('[Move]: Impossible move with simple piece');
+						return false;
+					}
+					if (this.data_game[p1i][p1j] == -2) {
+						//coup avec dame
+						if (Math.abs(p1i-p2i) == Math.abs(p1j-p2j)) {
+							//le coup est valide
+							var posList = this.NbBetween(player, p1i, p1j, p2i, p2j);
+							if (posList.length == 0) {
+								//pas de pion mangé
+								this.data_game[p1i][p1j] = 0;
+								this.data_game[p2i][p2j] = -2;
+								return {'player': 'player2', 'issue': 'simple', 'transform': false, 'pieceType': 'dame'};
+							}
+							if (posList.length == 1) {
+								//présence d'un pion
+								this.data_game[p1i][p1j] = 0;
+								this.data_game[p2i][p2j] = -2;
+								this.data_game[posList[0].i][posList[0].j] = 0;
+								this.eaten2 += 1;
+								if (this.isWon(player)) {
+									return {'player': 'player2', 'issue': 'eaten', 'transform': false, 'posEaten': posList[0], 'pieceType': 'dame', 'win': 'player2'};
+								}
+								return {'player': 'player2', 'issue': 'eaten', 'transform': false, 'posEaten': posList[0], 'pieceType': 'dame'};
+							}
+							//trop de pions présents
+							console.log('[Move]: Too much pieces NbBetween '+p1i+','+p1j+' and '+p2i+','+p2j);
+							return false;
+						}
+						console.log('[Move]: Impossible move with a dame');
+						return false;
+					}
+					console.log('[Move]: Invalid selection (inside move) at '+p1i+','+p1j);
+					return false;
+				}
+
+				//aucun joueur valide
+				console.log('[Move]: Invalid player');
+				return false;
+			}
+			else {console.log('[Move]: Not empty target at '+p2i+' '+p2j); return false;}
+		}
+		else {console.log('[Move]: Invalid selection at '+p1i+' '+p1j); return false;}
+	}
+
+	validMoveEating(player,p1i,p1j,p2i,p2j) {
+		if (this.validSelection(player, p1i, p1j)) {
+			if (this.data_game[p2i][p2j] == 0) {
+				//selection valide et pas de pion sur la case cible
+
+				if (player == this.player1) {
+					if (this.data_game[p1i][p1j] == 1) {
+						//pion normal
+						if (Math.abs(p2i - p1i) == 2 && Math.abs(p2j - p1j) == 2) {
+							//coup attaque valide
+							if (this.data_game[(p2i+p1i)/2][(p2j+p1j)/2] <= -1) {
+								//présence d'un pion ennemi
+								return true
+							}
+						}
+					}
+					if (this.data_game[p1i][p1j] == 2) {
+						//pion dame
+						if (Math.abs(p1i-p2i) == Math.abs(p1j-p2j)) {
+							//coup valide
+							if (this.NbBetween(player,p1i,p1j,p2i,p2j).length == 1) {
+								//présence d'un unique pion ennemi
+								return true
+							}
+						}
+					}
+				}
+
+				if (player == this.player2) {
+					if (this.data_game[p1i][p1j] == -1) {
+						//pion normal
+						if (Math.abs(p2i - p1i) == 2 && Math.abs(p2j - p1j) == 2) {
+							//coup attaque valide
+							if (this.data_game[(p2i+p1i)/2][(p2j+p1j)/2] >= 1) {
+								//présence d'un pion ennemi
+								return true
+							}
+						}
+					}
+					if (this.data_game[p1i][p1j] == -2) {
+						//pion dame
+						if (Math.abs(p1i-p2i) == Math.abs(p1j-p2j)) {
+							//coup valide
+							if (this.NbBetween(player,p1i,p1j,p2i,p2j).length == 1) {
+								//présence d'un unique pion ennemi
+								return true
+							}
+						}
+					}
+				}
+			}
+		}
+
+		return false
+	}
+
+	isWon(player) {
+		switch (player) {
+			case game.player1:
+				return this.eaten1 == 20;
+				break
+			case game.player2:
+				return this.eaten2 == 20;
+		}
+	}
+
+	//on rajoute une métode test
+	test() {
+		return "gali galou"
+	}
+
+
+}
+
+
+
+module.exports.Game2 = Game;
---
+++ server1.js
@@ -0,0 +1,263 @@
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http); //create a new instance of socket.io
+const Game = require('./class_game2.js').Game2; //importation de la classe Game
+
+//déclaration des variables essentielles
+var game = new Game(undefined,undefined);
+var started = false;
+
+//partie routage middleware
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/pageweb1.html');
+});
+app.get('/images/:image', function(req,res){
+  switch (req.params.image) {
+    case 'black':
+      res.sendFile(__dirname + '/images/black.png');
+      break
+    case 'gray':
+      res.sendFile(__dirname + '/images/gray.png');
+      break
+    case 'me1':
+      res.sendFile(__dirname + '/images/me1.png');
+      break
+    case 'you1':
+      res.sendFile(__dirname + '/images/you1.png');
+      break
+    case 'meD1':
+      res.sendFile(__dirname + '/images/meD1.png');
+      break
+    case 'youD1':
+      res.sendFile(__dirname + '/images/youD1.png');
+      break;
+  }
+});
+app.get('/data', (req,res) => {
+  res.send(game.data_game);
+});
+
+//partie socket
+io.on('connection', (socket) => {
+  //listen on the connection event
+  console.log(socket.id + ' connected');
+  //attribution des joueur lors de la connection
+  if (game.player1 == undefined) {
+    game.player1 = socket.id;
+    io.emit('chat message', '[info]: Joueur 1 connecté');
+  }
+  else {
+    if (game.player2 == undefined) {
+      game.player2 = socket.id;
+      io.emit('chat message', '[info]: Joueur 2 connecté');
+    }
+    else {console.log('Trop de joueurs en ligne')}
+  };
+
+  //récupération des données du jeu
+  socket.on('data', () => {
+    socket.emit('data',game.data_game);
+  });
+
+  socket.on('start', () => {
+    //permet à la partie de commencer si les deux joueurs sont prêts
+    //mieux que /start
+    console.log(socket.id + " ready");
+    switch (socket.id) {
+        case game.player1:
+          game.readyPlayer1 = true;
+          break
+        case game.player2:
+          game.readyPlayer2 = true;
+      }
+
+    //test pour démarrage de la partie
+    if (game.status != 'started') {
+      console.log(game.status);
+      if (game.readyPlayer1 && game.readyPlayer2) {
+        if (game.status == 'interrupted') {
+          game.status = 'started';
+          io.emit('chat message', 'La partie recommence !');
+        }
+        if (game.status == 'stoped') {
+          game.status = 'started';
+          io.emit('chat message', 'La partie commence !');
+          io.emit('chat message', 'Le Joueur 1 commence');
+        }
+      }
+    }
+  });
+
+  socket.on('pass', () => {
+    //permet aux joueurs de passer leur tour
+    if (game.status == 'started') {
+      if (game.turn == game.getPlayer(socket.id)) {
+        switch (socket.id) {
+          case game.player1:
+            game.turn = 'player2';
+            game.blows = [];
+            io.to(game.player1).emit('chat message','Tour passé');
+            io.to(game.player2).emit('chat message','A votre tour de jouer !');
+            break
+          case game.player2:
+            game.turn = 'player1';
+            game.blows = [];
+            io.to(game.player2).emit('chat message','Tour passé');
+            io.to(game.player1).emit('chat message','A votre tour de jouer !');
+        }
+      }
+    }
+    else {
+      switch (socket.id) {
+        case game.player1:
+          if (game.status == 'interrupted') {
+            socket.emit('chat message','En attente du Joueur 2 déconnecté ...')
+          }
+        case game.player2:
+          if (game.status == 'interrupted') {
+            socket.emit('chat message','En attente du Joueur 1 déconnecté ...')
+          }
+      }
+    }
+  });
+
+
+  socket.on('chat message', (msg) => {
+    //écrit dans le chat pour tout le monde
+    switch (socket.id) {
+        case game.player1:
+          io.emit('chat message', 'Joueur 1 : ' + msg);
+          break
+        case game.player2:
+          io.emit('chat message', 'Joueur 2 : ' + msg);
+    };
+  });
+
+  socket.on('move', (data) => { //on reçoit data de la forme {'i1': i1, 'j1': j1, 'i2': i2, 'j2': j2 } (1 vers 2)
+    //réception des données du move
+    if (game.status == 'started') {
+      if (game.player1 == undefined || game.player2 == undefined) {
+        //un joueur est déconnecté
+        switch (socket.id) {
+          case game.player1 :
+            socket.emit('chat message','En attente du Joueur 2 ...')
+            break
+          case game.player2 :
+            socket.emit('chat message','En attente du Joueur 1 ...')
+        }
+      }
+      else {
+        // les joueur sont tous deux connectés
+        if (game.getPlayer(socket.id) == game.turn) {
+        //résultat du move
+        if (game.blows.length == 0) {
+          //aucun coup stacké
+          result = game.move(socket.id, data.i1, data.j1, data.i2, data.j2);
+          if (result) {
+            sended_data = {...data,...result};
+            io.emit('move', sended_data);
+            io.emit('chat message', "Action effectuée");
+            if (result.win != undefined) {
+              switch (result.win) {
+                case 'player1':
+                  io.emit('chat message','Partie terminée, le Joueur 1 a gagné :)')
+                  break
+                case 'player2':
+                  io.emit('chat message','Partie terminée, le Joueur 2 a gagné :)')
+              }
+            }
+            //gestion des coups multiples
+            if (result.issue == 'eaten') {
+              game.blows.push({'i':data.i1, 'j':data.j1});
+              game.blows.push({'i':data.i2, 'j':data.j2});
+            }
+            else {
+              switch (socket.id) {
+                case game.player1:
+                  game.turn = 'player2';
+                  io.to(game.player2).emit('chat message','A votre tour de jouer !');
+                  break
+                case game.player2:
+                  game.turn = 'player1';
+                  io.to(game.player1).emit('chat message','A votre tour de jouer !');
+                  break
+              }
+            }
+          }
+          else {
+          socket.emit('chat message', "Action impossible");
+          }
+        }
+        else {
+          //il y a des coups stackés
+          var n = game.blows.length;
+          if (data.i1 == game.blows[n-1].i && data.j1 == game.blows[n-1].j) {
+            //manque le fait de ne pas revenir sur une case déjà visitée
+            //le dernier pion est bien réutilisé
+            //faire fonction test
+            if (game.validMoveEating(socket.id,data.i1, data.j1, data.i2, data.j2)) {
+              //le coup est vérifié
+              result = game.move(socket.id, data.i1, data.j1, data.i2, data.j2);
+              if (result) { //seconde vérification
+                sended_data = {...data,...result};
+                io.emit('move', sended_data);
+                io.emit('chat message', "Action effectuée");
+                game.blows.push({'i':data.i2, 'j':data.j2});
+                if (result.win != undefined) {
+                  switch (result.win) {
+                    case 'player1':
+                      io.emit('chat message','Partie terminée, le Joueur 1 a gagné :)')
+                      break
+                    case 'player2':
+                      io.emit('chat message','Partie terminée, le Joueur 2 a gagné :)')
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+      else {
+        socket.emit('chat message', "Ce n'est pas à votre tour de jouer");
+      }
+      }
+    }
+    else {
+      if (game.status == 'interrupted') {
+        socket.emit('chat message', "Action impossible, partie interrompue ...");
+      }
+      else {
+        socket.emit('chat message', "Action impossible, partie non commencée");
+      }
+    }
+  });
+
+  //désattribution des joueurs lors de la déconnection
+  socket.on('disconnect', () => {
+    console.log(socket.id + ' disconnected');
+    switch (socket.id) {
+      case game.player1:
+        game.player1 = undefined;
+        io.emit('chat message', '[info]: Joueur 1 déconnecté');
+        if (game.status != 'stoped') {
+          game.status = 'interrupted'
+        }
+        game.readyPlayer1 = false;
+        break
+      case game.player2:
+        game.player2 = undefined;
+        io.emit('chat message', '[info]: Joueur 2 déconnecté');
+        if (game.status != 'stoped') {
+          game.status = 'interrupted'
+        }
+        game.readyPlayer2 = false;
+        break
+    }
+  });
+});
+
+
+//partie listen
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
